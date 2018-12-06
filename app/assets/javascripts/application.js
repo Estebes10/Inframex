87be6fcf8a65dbc0ac861df638af7a1505bfb4dc
Salomon <salomon.olivera@hotmail.com>
2018-12-05 21:05:26 -0600

    Login page done, needs to be responsive tho

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require particles
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Session routes
+  get '/login', to: 'session#new', as: :new_session
 end
