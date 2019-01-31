d48f17e01286df165313a0938da06467af55d3b0
Bernardo Gómez <a01209704@itesm.mx>
2019-01-31 04:59:05 +0000

    fixed issue with datatable initialization

--- app/assets/javascripts/user_projects.js
+++ app/assets/javascripts/user_projects.js
@@ -5,12 +5,6 @@ $( document ).on('ready turbolinks:load', function() {
         }
     });
 
-    $('#user-projects-new-datatable').DataTable({
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
-        }
-    });
-
     $('#users-projects-datatable tbody').on('click', 'button.delete-user-project', function (e) {
         e.preventDefault(e);
         var deleteButton = $(this);
